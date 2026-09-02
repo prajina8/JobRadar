9c6bc5acd2bb655c745c4ad6306ccc7f0c7e9e99
prajina-Adhikari <aprajina45@gmail.com>
2026-09-02 22:22:37 +0545

    changes

--- frontend/src/pages/Jobs.jsx
+++ frontend/src/pages/Jobs.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import React,{ useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
